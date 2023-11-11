3b80071cf620d1514d131d0200d1b50ab894c828
bernal9913 <carlosbernal@Carloss-MacBook-Air.local>
2023-11-11 12:46:47 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Calendario de Eventos</h1>
+    <event-calendar :events="eventList" />
+  </div>
+</template>
+
+<script>
+import EventCalendar from './EventCalendar.vue';
+
+export default {
+  data() {
+    return {
+      eventList: [
+        {
+          date: '2023-11-15 16:00',
+          title: 'Agenda ocupada',
+          description: 'Sumergirme en mi propia miseria',
+        },
+        {
+          date: '2023-11-15 16:30',
+          title: 'Agenda ocupada',
+          description: 'Contemplar el abismo',
+        },
+        {
+          date: '2023-11-15 17:00',
+          title: 'Agenda ocupada',
+          description: 'Solucionar la hambruna mundial sin decirselo a nadie',
+        },
+        {
+          date: '2023-11-15 17:30',
+          title: 'Agenda ocupada',
+          description: 'Danza y ejercicio',
+        },
+        {
+          date: '2023-11-15 18:30',
+          title: 'Agenda ocupada',
+          description: 'Cena conmigo mismo; NOTA: esa no se cancela',
+        },
+        {
+          date: '2023-11-15 19:00',
+          title: 'Agenda ocupada',
+          description: 'Luchar con el odio que me tengo ',
+        }
+      ],
+    };
+  },
+  components: {
+    EventCalendar,
+  },
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 20px;
+}
+
+</style>
---
+++ src/EventCalendar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="event-calendar">
+    <table>
+      <thead>
+      <tr>
+        <th>Fecha</th>
+        <th>Título</th>
+        <th>Descripción</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(event, index) in events" :key="index">
+        <td>{{ event.date }}</td>
+        <td>{{ event.title }}</td>
+        <td>{{ event.description }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    events: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.event-calendar {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ccc;
+  padding: 8px;
+}
+
+th {
+  background-color: #f0f0f0;
+  font-weight: bold;
+}
+</style>
